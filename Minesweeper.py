13e8c9b640a8cffdf12646d629b64315515eaee8
AshBoomstick1 <143759985+AshBoomstick1@users.noreply.github.com>
2023-11-04 19:06:56 -0400

    Added optional board sizes

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,6 +1,9 @@
 import random
+import math
+import datetime
 #Sets of the game
 placing_flag = 2
+tile_num = int(input("\nHow many tiles in the grid?: "))
 can_remove_flag = input("\nDo want to be able to remove flag? (y/n): ")
 if can_remove_flag == "y":
     can_remove_flag = True
@@ -18,15 +21,19 @@ global uncover_0_count
 uncover_0_count = 0
 #defines the tiles
 class Tile:
-    def __init__(self, position_x, position_y, is_bomb = False, num_of_surrounding_bombs = 0, is_covered = True, is_flagged = False):
+    def __init__(self, position_x, position_y, id, is_bomb = False, num_of_surrounding_bombs = 0, is_covered = True, is_flagged = False, is_checked = False):
         self.position_x = position_x
         self.position_y = position_y
+        self.id = id
         self.is_bomb = is_bomb
         self.num_of_surrounding_bombs = num_of_surrounding_bombs
         self.is_covered = is_covered
         self.is_flagged = is_flagged
+        self.is_checked = is_checked
     #This shows what the tiles will look like on the board
     def __repr__(self):
+        #return_string = str(self.position_x) + ", " + str(self.position_y)
+        #return return_string
         global placing_flag
         global current_tile_x
         global current_tile_y
@@ -37,7 +44,7 @@ class Tile:
             else:
                 if placing_flag == 1:
                     if self.is_flagged == False:
-                        return self.find_surrounding_bomb_for_tile()
+                        return find_surrounding_bomb_for_tile(self)
                     else:
                         return "X"
                 elif placing_flag == 2:
@@ -45,19 +52,18 @@ class Tile:
                         if self.is_flagged == True:
                             return "X"
                         else:
-                            return self.find_surrounding_bomb_for_tile()
+                            return find_surrounding_bomb_for_tile(self)
                     else:
-                        return self.find_surrounding_bomb_for_tile()
+                        return find_surrounding_bomb_for_tile(self)
                 elif placing_flag == 3:
                     if self.is_flagged == False:
-                        return self.find_surrounding_bomb_for_tile()
+                        return find_surrounding_bomb_for_tile(self)
                     else:
                         return "X"
         else:
             return " "
     #This checks weather the tile is a bomb and flags as well as if you can remove flags.
     def uncover(self):
-        print("YYWEG")
         global uncovered_count
         global turn
         global current_tile_x
@@ -72,7 +78,6 @@ class Tile:
         while turn == 0 and self.is_bomb == True and un_cover_again == True:
             self.pick_new_bomb()
         un_cover_again = False
-
         if can_remove_flag == False:
             input_flag = input("Add flag or uncover tile? (1 to uncover, 2 to flag):")
             while type(input_flag) != str or input_flag == "":
@@ -92,32 +97,31 @@ class Tile:
         for tile in tile_list:
             if tile.is_covered == False:
                 if tile.position_x == current_tile_x and tile.position_y == current_tile_y:
-                    print(show_board())
+                    print(print_board())
                     new_pick()
 
         turn += 1
 
         current_tile_if_bomb = self.is_bomb
         if self.is_bomb == True:
-            print("BOMB")
             if placing_flag == 1:         
                 print("YOU LOSE: UNCOVERED MINE")
                 end_game()
             elif placing_flag == 2:
                 self.is_flagged = True
                 self.is_covered = False
-                print(show_board())
+                print(print_board())
                 new_pick()
             elif placing_flag == 3:
                 self.is_flagged = False
                 self.is_covered = True
-                print(show_board())
+                print(print_board())
                 new_pick()
         else:
             if placing_flag == 1:
                 self.is_covered = False
                 if is_tile_0(self) == True:
-                    find_surrounding_0s(self, 6)
+                    find_surrounding_0s(self)
 
             elif placing_flag == 2:
                 if can_remove_flag == True:
@@ -130,194 +134,224 @@ class Tile:
                 self.is_flagged = False
                 self.is_covered = True
                    
-            print(show_board())
+            print(print_board())
             new_pick()
+
     #picks new bombs if your first tile was a bomb
     def pick_new_bomb(self):
         global bomb_list
         self.is_bomb = False
         bomb_list = []
-        select_bombs(num_of_bombs, tile_list)
+        remaining_list = [tile_dict[tile] for tile in tile_dict]
+        select_bombs(num_of_bombs, remaining_list)
+    
+    def check(self):
+        self.is_checked == True
 
-    #finds the number the tile should be
-    def find_surrounding_bomb_for_tile(self):
+#finds the number the tile should be
+def find_surrounding_bomb_for_tile(current_tile):
         bomb_count = 1
-        for tile in surrounding_tiles[self]:
+        for tile_pos in surrounding_tiles[str(tile_pos_dict[str(current_tile.id)][0])]:
+            tile = tile_pos_dict[str(tile_pos)][1]
             if tile.is_bomb:
                 bomb_count += 1
         return str(bomb_count - 1)
+
 #ends the game
 def end_game():
     print("Your Score is: " + str(uncovered_count + turn))
     for bomb in tile_list:
         if bomb.is_bomb == True:
             print("Mines were at: " + str(bomb.position_y) + ", " + str(bomb.position_x))
-    print(bomb_list)
     exit()
 
 #Finds the tiles around it that are 0, is used in the mass clearing of 0s
-def find_surrounding_0s(tile, i):
-    if i != 0:
-        surounding_tiles = find_surounding_tiles(tile)
-        for tile1 in surounding_tiles:
-            tile1.is_covered = False
-            if is_tile_0(tile1) == True:
-                find_surrounding_0s(tile1, i-1)
+
+def find_surrounding_0s(tile):
+    for tile_pos in find_tile_in_surrounding_tiles(tile):
+        stile = tile_pos_dict[str(tile_pos)][1]
+        if stile.is_checked == False:
+            stile.is_checked = True
+            stile.is_covered = False
+            if is_tile_0(stile):
+                find_surrounding_0s(stile)
+
+def find_tile_in_surrounding_tiles(tile):
+    tile_id = tile.id
+    stile = tile_pos_dict[tile_id][0]
+    return surrounding_tiles[stile]
     
 #checks is a tile is 0
 def is_tile_0(tile):
     bomb_count = 0
-    for tile in surrounding_tiles[tile]:
-        if tile.is_bomb:
+    for tile1 in surrounding_tiles[str(tile_pos_dict[str(tile.id)][0])]:
+        if tile_pos_dict[str(tile1)][1].is_bomb:
             bomb_count += 1
     if bomb_count == 0:
         return True
     else:
         return False
+    
 
 #finds all the suroundings tiles
 def find_surounding_tiles(self):
     return_list = []
-    for tile in surrounding_tiles[self]:
+    for tile in surrounding_tiles[str(tile_pos_dict[str(self.id)][0])]:
         return_list.append(tile)
     return return_list
 
-#does the mass clearing of 0s
-def uncover_0_field(tile):
-    global uncover_0_count
-    print(uncover_0_count)
-    uncover_0_count += 1
+def cal_id(x, xd, y, yd):
+    return str(x + xd) + str(y + yd)
+
+#find the best pair of factors for the board aspect ratio
+def find_factors(n):
     return_list = []
-    surrounding_tiles_list = find_surounding_tiles(tile)
-    for tile in surrounding_tiles_list:
-        tile.is_covered = False
-        if is_tile_0(tile):
-            return_list.append(tile)
-    if uncover_0_count < 12:
-        for tile1 in return_list:
-            uncover_0_field(tile1)
-    else:
-        return return_list
-
-#sets of the tiles cords
-tile1 = Tile(1, 1)
-tile2 = Tile(1, 2)
-tile3 = Tile(1, 3)
-tile4 = Tile(1, 4)
-tile5 = Tile(1, 5)
-tile6 = Tile(2, 1)
-tile7 = Tile(2, 2)
-tile8 = Tile(2, 3)
-tile9 = Tile(2, 4)
-tile10 = Tile(2, 5)
-tile11 = Tile(3, 1)
-tile12 = Tile(3, 2)
-tile13 = Tile(3, 3)
-tile14 = Tile(3, 4)
-tile15 = Tile(3, 5)
-tile16 = Tile(4, 1)
-tile17 = Tile(4, 2)
-tile18 = Tile(4, 3)
-tile19 = Tile(4, 4)
-tile20 = Tile(4, 5)
-tile21 = Tile(5, 1)
-tile22 = Tile(5, 2)
-tile23 = Tile(5, 3)
-tile24 = Tile(5, 4)
-tile25 = Tile(5, 5)
+    #makes a list of factors
+    for num in range(1, math.ceil(n/2) + 1):
+        if num == 0:
+            num += 1
+        if n % num ==0:
+            return_list.append(num)
+    if len(return_list) == 1:
+        return [1, 1]
+    
+    #makse a list of pairs of all the factors
+    pair_list = []
+    for num in return_list:
+        if int(num) == 1:
+            return_list.pop(return_list.index(num))
+    for num in range(len(return_list)):
+        if return_list[num] * return_list[num] == n:
+            return [return_list[num], return_list[num]]
+        if return_list[num] != return_list[-1]:
+            pair_list.append([return_list[num], return_list[num + 1]])
+    for num in range(len(return_list) - 1):
+        if return_list[num] != return_list[-2] and return_list[num] != return_list[-1]:
+            pair_list.append([return_list[num], return_list[num + 2]])
 
 
-#the list of all tiles
-tile_list = [
-         tile1,tile2,tile3,tile4,tile5,
-         tile6,tile7,tile8,tile9,tile10,
-         tile11,tile12,tile13,tile14,tile15,
-         tile16,tile17,tile18,tile19,tile20,
-         tile21,tile22,tile23,tile24,tile25
-        ]
+    num_removed = 0
+    finished_list = [pair for pair in pair_list]
 
-#a dict with all surounding tiles
-surrounding_tiles = {tile1: [tile2, tile7, tile6], 
-                     tile2: [tile1, tile3, tile6, tile7, tile8], 
-                     tile3:[tile2, tile4, tile7, tile8, tile9], 
-                     tile4: [tile3, tile5, tile8, tile9, tile10], 
-                     tile5: [tile4, tile9, tile10], 
-                     tile6: [tile1, tile2, tile7, tile11, tile12],
-                     tile7: [tile1, tile2, tile3, tile6, tile8, tile11, tile12, tile13], 
-                     tile8: [tile2, tile3, tile4, tile7, tile9, tile12, tile13, tile14], 
-                     tile9: [tile3, tile4, tile5, tile8, tile10, tile13, tile14, tile15], 
-                     tile10: [tile4, tile5, tile9, tile14, tile15], 
-                     tile11: [tile6, tile7, tile12, tile16, tile17], 
-                     tile12: [tile6, tile7, tile8, tile11, tile13, tile16, tile17, tile18], 
-                     tile13: [tile7, tile8, tile9, tile12, tile14, tile17, tile18, tile19],
-                     tile14: [tile8, tile9, tile10, tile13, tile15, tile18, tile19, tile20],
-                     tile15: [tile9, tile10, tile14, tile19, tile20],
-                     tile16: [tile11, tile12, tile17, tile21, tile22],
-                     tile17: [tile11, tile12, tile13, tile16, tile18, tile21, tile22, tile23],
-                     tile18: [tile12, tile13, tile14, tile17, tile19, tile22, tile23, tile24],
-                     tile19: [tile13, tile14, tile15, tile18, tile20, tile23, tile24, tile25],
-                     tile20: [tile14, tile15, tile19, tile24, tile25],
-                     tile21: [tile16, tile17, tile22],
-                     tile22: [tile16, tile17, tile18, tile21, tile23],
-                     tile23: [tile17, tile18, tile19, tile22, tile24],
-                     tile24: [tile18, tile19, tile20, tile23, tile25],
-                     tile25: [tile19, tile20, tile24]}
+    #removes unnessicary items
+    for pair in pair_list:
+
+        if pair[0] * pair[1] != n:
+
+            finished_list.pop(finished_list.index(pair))
+
+            num_removed += 1
+
+    difference_list = []
+    #fills in the difference list which is a list of differences between each pair
+    for pair in finished_list:
+        difference_list.append(pair[1] - pair[0])
+    #finds the smallest difference
+
+    for difference in difference_list:
+        for difference1 in difference_list:
+            if difference > difference1:
+                difference_list.pop(difference_list.index(difference))
+
+    for pair in finished_list:
+        if pair[1] - pair[0] == difference_list[0]:
+            return pair
+        
+tile_dict = {}
+tile_pos_dict = {}
+def make_tiles(num_of_tiles):
+    global row_count
+    global col_count
+    factor_list = find_factors(num_of_tiles)
+    row_count = factor_list[0]
+    col_count = factor_list[1]
+    tile_num = 0
+    for row in range(row_count):
+        for col in range(col_count):
+            tile_num += 1
+            var_name = "tile" + str(tile_num)
+            tile_dict[var_name] = Tile(row + 1, col + 1, cal_id(row + 1, 0, col + 1, 0))
+    for item in tile_dict:
+        tile_pos_dict["{x}{y}".format(x=tile_dict[item].position_x, y=tile_dict[item].position_y)] = [item, tile_dict[item]]
+
+
+make_tiles(tile_num)
+
+surrounding_tiles = {}
+def find_surrounding_tiles():
+    for tile in tile_dict:
+        x = tile_dict[tile].position_x
+        y = tile_dict[tile].position_y
+        nearby_tiles = []
+
+        if y > 1:
+            nearby_tiles.append(cal_id(x, 0, y, -1))
+            if x < row_count:
+                nearby_tiles.append(cal_id(x, 1, y, -1))
+            if x > 1:
+                nearby_tiles.append(cal_id(x, -1, y, -1))
+        
+        if x > 1:
+            nearby_tiles.append(cal_id(x, -1, y, 0))
+            if y < col_count:
+                nearby_tiles.append(cal_id(x, -1, y, 1))
+
+        if x < row_count:
+            nearby_tiles.append(cal_id(x, 1, y, 0))
+            if y < col_count:
+                nearby_tiles.append(cal_id(x, 1, y, 1))
+        
+        if y < col_count:
+            nearby_tiles.append(cal_id(x, 0, y, 1))
+        surrounding_tiles[tile] = nearby_tiles
+
+find_surrounding_tiles()
+tile_list = [tile_dict[tile] for tile in tile_dict]
 
 
 bomb_list = []
 for tile in tile_list:
     if tile.is_bomb == True:
         bomb_list.append(tile)
+        
 #selects bombs
 def select_bombs(bomb_count, tlist):
     if bomb_count != 0:
-        new_bomb = tlist[random.randint(0, 24)]
+        new_bomb = tlist[random.randint(0, len(remaining_list)) - 1]
         if new_bomb.is_bomb == True:
-            select_bombs(bomb_count, tlist)
+            select_bombs(bomb_count, remaining_list)
         else:
             new_bomb.is_bomb = True
+            tlist.pop(tlist.index(new_bomb))
             select_bombs(bomb_count - 1, tlist)
-select_bombs(num_of_bombs, tile_list)
+remaining_list = [tile_dict[tile] for tile in tile_dict]            
+select_bombs(num_of_bombs, remaining_list)
 
-#board
-def show_board():
-    board = """
-Turn: {turn}
- 0  1  2  3  4  5 (X)
- 1 [{tile1}][{tile2}][{tile3}][{tile4}][{tile5}]
- 2 [{tile6}][{tile7}][{tile8}][{tile9}][{tile10}]
- 3 [{tile11}][{tile12}][{tile13}][{tile14}][{tile15}]
- 4 [{tile16}][{tile17}][{tile18}][{tile19}][{tile20}]
- 5 [{tile21}][{tile22}][{tile23}][{tile24}][{tile25}]
-(Y)
-""".format(turn=turn,
-  tile1=tile1,
-  tile2=tile2, 
-  tile3=tile3,
-  tile4=tile4,
-  tile5=tile5,
-  tile6=tile6,
-  tile7=tile7, 
-  tile8=tile8,
-  tile9=tile9,
-  tile10=tile10,
-  tile11=tile11,
-  tile12=tile12,
-  tile13=tile13,
-  tile14=tile14,
-  tile15=tile15,
-  tile16=tile16,
-  tile17=tile17,
-  tile18=tile18,
-  tile19=tile19,
-  tile20=tile20,
-  tile21=tile21,
-  tile22=tile22,
-  tile23=tile23,
-  tile24=tile24,
-  tile25=tile25)
-    return board
+
+
+#def select_mines()
+
+
+def print_board():
+    def print_top_row():
+        return_string = " 0 "
+        for col in range(1, col_count + 1):
+            return_string += "  {col} ".format(col=col)
+        return return_string + " X\n"
+    print(print_top_row())
+    for row in range(1, row_count + 1):
+        def print_rows():
+            return_string = " {row} ".format(row=row)
+            for col in range(1, col_count + 1):
+                i = cal_id(row, 0, col, 0)
+                return_string += " [{n}]".format(n=tile_pos_dict[i][1])
+            print(return_string + " \n")
+        print_rows()
+    print(" Y\n")    
+
+
 #starts game
+start_time = datetime.datetime.now()
 turn  += 1
 x_input_list = [0]
 y_input_list = [0]
@@ -332,19 +366,22 @@ def new_pick():
     for tile in tile_list:
         if tile.is_covered == False and tile.is_bomb == False:
             uncovered_tile_count += 1
-    if uncovered_count == 25 or uncovered_tile_count == 25 - num_of_bombs:
+    if uncovered_count == len(tile_dict) or uncovered_tile_count == len(tile_dict) - num_of_bombs:
+        end_time = datetime.datetime.now()
+        time = str(end_time - start_time)
+        time = time[2:7]
         print("YOU WIN!")
-        print("YOU WON IN {turn} TURNS".format(turn=turn))
+        print("YOU WON IN {time} with {turn} TURNS".format(time=time, turn=turn))
     else:
 
         input_cord_x = input("Enter the X cord of the tile you want to uncover: ")
-        while type(input_cord_x) != str or input_cord_x == "" or int(input_cord_x) < 1 or int(input_cord_x) > 5:
+        while type(input_cord_x) != str or input_cord_x == "" or int(input_cord_x) < 1 or int(input_cord_x) > col_count:
             input_cord_x = input("Enter a number between 0 and 6 as your X cord: ") 
 
         x_input_list.append(input_cord_x)
 
         input_cord_y = input("Enter the Y cord of the tile you want to uncover: ")  
-        while input_cord_y == "" or int(input_cord_y) < 1 or int(input_cord_y) > 5:
+        while input_cord_y == "" or int(input_cord_y) < 1 or int(input_cord_y) > row_count:
             input_cord_y = input("Enter a number between 0 and 6 as your Y cord: ")
 
         y_input_list.append(input_cord_y)
@@ -354,5 +391,5 @@ def new_pick():
                 tile.uncover()
 
 
-print(show_board())
+print(print_board())
 new_pick()
